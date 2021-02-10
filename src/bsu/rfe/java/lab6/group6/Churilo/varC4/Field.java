b084e4da76a1e8f774f345edbf7f395203373a10
Ignat Churilo <i.ignatch.com@mail.ru>
2021-02-10 13:32:05 +0300

    Class Field Create

--- src/bsu/rfe/java/lab6/group6/Churilo/varC4/Field.java
+++ src/bsu/rfe/java/lab6/group6/Churilo/varC4/Field.java
@@ -1,16 +1,19 @@
 package bsu.rfe.java.lab6.group6.Churilo.varC4;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class Field extends JPanel {
     private boolean paused;
-    private final int blocksInRow = 20;
+    public final int blocksInRow = 20;
+    public final int MAX_COUNT_OF_BLOCKS = blocksInRow * blocksInRow - 4;
 
     private BounclingBall ball;
 
-    //private Blo
+    private BreakableBlock[][] blocks = new BreakableBlock[blocksInRow][blocksInRow];
 
     private Timer repaintTimer = new Timer(10, new ActionListener() {
         public void actionPerformed(ActionEvent e) {
@@ -18,5 +21,33 @@ public class Field extends JPanel {
         }
     });
 
+    public Field(){
+        setBackground(Color.WHITE);
+        repaintTimer.start();
+    }
 
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D canvas = (Graphics2D) g;
+        //ball.paint(canvas);
+    }
+
+    public void addBall(){
+
+    }
+
+    public synchronized void pause() {
+        paused = true;
+    }
+    
+    public synchronized void resume(){
+        paused = false;
+        notifyAll();
+    }
+
+    public synchronized void canMove(BounclingBall ball) throws  InterruptedException{
+        if (paused){
+            wait();
+        }
+    }
 }
